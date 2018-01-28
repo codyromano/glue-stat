71de4dac98eb57e58b18eacd20295422664aa66a
Cody Romano <codyromano@gmail.com>
2018-01-27 18:54:35 -0800

    Integration test for creating a task

--- test/integration/task_flow_test.rb
+++ test/integration/task_flow_test.rb
@@ -5,9 +5,21 @@ class TaskFlowTest < Capybara::Rails::TestCase
     @one = tasks :one
     @two = tasks :two
   end
+
   test "tasks listed on index page" do
     visit tasks_path
     assert page.has_content?(@one.title)
     assert page.has_content?(@two.title)
+  end
+
+  test "creating a task" do
+    visit tasks_path
+    click_on "Create a Glue Stat"
+
+    fill_in "Title", with: "My Glue Stat Title"
+    click_on "Create Glue Stat"
+
+    assert_current_path task_path(Task.last)
+    assert page.has_content?("My Glue Stat Title")
   end
 end
